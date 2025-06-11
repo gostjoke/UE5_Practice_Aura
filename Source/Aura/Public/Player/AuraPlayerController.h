944cd0af03359ad471d8e790883d84245010da78
Will <gostjoketw@gmail.com>
2025-06-11 01:19:09 -0700

    Enhanced Input

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// This is a practice project, Free to use!
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
