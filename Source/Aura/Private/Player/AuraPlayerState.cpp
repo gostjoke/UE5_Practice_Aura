602453cba48fe5e988dde290a3e089ee450b0d4f
Will <gostjoketw@gmail.com>
2025-06-15 14:13:50 -0700

    The Player State

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// This is a practice project, Free to use!
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+    NetUpdateFrequency = 100.f; // Set a custom network update frequency
+}
---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,20 @@
+// This is a practice project, Free to use!
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerState();
+};
+
